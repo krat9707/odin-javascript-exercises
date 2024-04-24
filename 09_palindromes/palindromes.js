bb7b01b595a49f57eedaf7dc65f02355b6bbcecc
krat <kashyapbaraiya9707@gmail.com>
2024-04-25 01:15:46 +0530

    Solve 09 exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const rangeOfChars = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    let newString = str
+        .toLowerCase()
+        .split('')
+        .filter((character) => rangeOfChars.includes(character))
+        .join('');
+    
+    const reverseString = newString.split('').reverse().join('');
+
+    return newString === reverseString;
 };
+
+function isValidLetter(ch)
+{
+    if(ch.charCodeAt(0) >= 97 && ch.charCodeAt(0) <= 122)
+        return true;
+    return false;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
