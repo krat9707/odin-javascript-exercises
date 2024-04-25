74547e4fb9867207501b152b72db9feee07fdeda
krat <kashyapbaraiya9707@gmail.com>
2024-04-26 00:47:41 +0530

    solve 12 exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(peeps) {
+    return peeps.reduce((maxAgePerson, currPerson) => {
+        const maxAge = getAge(maxAgePerson.yearOfBirth, maxAgePerson.yearOfDeath);
+        const currAge = getAge(currPerson.yearOfBirth, currPerson.yearOfDeath);
 
+        return maxAge < currAge ? currPerson : maxAgePerson;
+    });
 };
+
+const getAge = function (birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  };
 
 // Do not edit below this line
 module.exports = findTheOldest;
