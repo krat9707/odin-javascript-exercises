2e5f033eb47e673133192d1952ad8eb325d30fe0
krat <kashyapbaraiya9707@gmail.com>
2024-04-26 00:09:05 +0530

    Solve 10 exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = parseInt(n);
+    if(n < 0)
+        return "OOPS";
 
+    let prev = 0;
+    let prev2 = 1;
+    let temp;
+
+    if(n == 0)
+        return 0;
+    if(n <= 2)
+        return 1;
+
+    for(let i = 2; i <= n; ++i)
+    {
+        temp = prev + prev2;
+        prev = prev2;
+        prev2 = temp;
+    }
+
+    return prev2;
 };
 
 // Do not edit below this line
